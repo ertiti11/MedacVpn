4f0be0679eec2af114453a436dbdd8a2b77a04cb
ertiti11 <vatitiprieto11@gmail.com>
2023-09-28 14:56:15 +0200

    animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,14 @@ module.exports = {
 			colors:{
 				'buttonG1' : "#d83333",
 				'buttonG2' :"#f041ff"
+			},
+			animation:{
+				wiggle: {
+					'0%, 100%': { transform: 'rotate(-3deg)' },
+					'50%': { transform: 'rotate(3deg)' },
+				  }
 			}
+
 		},
 	},
 	plugins: [],
