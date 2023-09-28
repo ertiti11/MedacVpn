cd72d1cafabf7ac0ac6ff9f6563e1933d0af8f24
ertiti11 <vatitiprieto11@gmail.com>
2023-09-29 00:11:58 +0200

    backend added, login, register

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
+import { createAccessToken } from "../libs/jwt.js";
 
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
@@ -10,30 +10,57 @@ export const register = async (req, res) => {
 
     const newUser = new User({ username, email, password: passwordHash });
     const newUserSaved = await newUser.save();
-    jwt.sign(
-      { id: newUserSaved._id },
-      "secret123",
-      { expiresIn: "1d" },
 
-      (err, token) => {
-        if (err) {
-          console.log(err);
-        } else {
-          res.json({ token });
-        }
-      }
-    );
-    // res.json({
-    //   id: newUserSaved._id,
-    //   username: newUserSaved.username,
-    //   email: newUserSaved.email,
-    //   createdAt: newUserSaved.createdAt,
-    //   updatedAt: newUserSaved.updatedAt,
-    // });
+    const token = await createAccessToken({ id: newUserSaved._id });
+
+    res.cookie("token", token);
+
+    res.json({
+      id: newUserSaved._id,
+      username: newUserSaved.username,
+      email: newUserSaved.email,
+      createdAt: newUserSaved.createdAt,
+      updatedAt: newUserSaved.updatedAt,
+    });
   } catch (error) {
-    res.json("Error: " + error);
+    res.status(500).json("Error: " + error.message);
   }
 };
-export const login = (req, res) => {
-  res.send("login");
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const userFound = await User.findOne({ email });
+
+    if (!userFound) return res.status(400).json({ message: "user not found" });
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+
+    if (!isMatch)
+      return res.status(400).json({ message: "incorrect password" });
+
+    const token = await createAccessToken({ id: userFound._id });
+
+    res.cookie("token", token);
+
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json("Error: " + error.message);
+  }
+};
+
+export const logout = async (req, res) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    secure: true,
+    expires: new Date(0),
+  });
+  return res.sendStatus(200);
 };
