06e0a9ba6728138f1e2f444ee1fdf685307de44b
ertiti11 <vatitiprieto11@gmail.com>
2023-12-20 00:30:16 +0100

    control

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,8 +1,7 @@
-const SECRET = "secret123"
-const MONGODB_URI_PROD = "mongodb://localhost:27017/medacVPN"
-const MONGODB_URI_DEV = "mongodb://localhost:27017/medacVPN-dev"
+const SECRET = "secret123";
+const MONGODB_URI_PROD =
+  "mongodb+srv://admin:admin@medacvpn.2m42csq.mongodb.net/?retryWrites=true&w=majority";
+const MONGODB_URI_DEV =
+  "mongodb+srv://admin:admin@medacvpn.2m42csq.mongodb.net/medacvpnapi-dev?retryWrites=true&w=majority";
 
-
-
-
-module.exports = { SECRET, MONGODB_URI_PROD, MONGODB_URI_DEV }
+module.exports = { SECRET, MONGODB_URI_PROD, MONGODB_URI_DEV };
