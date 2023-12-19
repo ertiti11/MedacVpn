e1c16c338679f4dbc3d3ce8131a7da32f5ee0436
ertiti11 <vatitiprieto11@gmail.com>
2023-12-19 21:56:35 +0100

    helmet-express added

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,8 +3,11 @@ const morgan = require("morgan");
 const authRoutes = require("./routes/auth.routes.js");
 const cookieParser = require("cookie-parser");
 const { Logger } = require("./controllers/log.controller.js");
+import helmet from "helmet";
 
 const app = express();
+app.use(helmet());
+
 app.use(
   morgan(
     ":remote-addr - :method - :url - :status - :res[content-length] - :response-time ms",
